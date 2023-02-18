aea5ba2341da5145bf94060c68065be87436b215
WRICHY00 <fredricklincoln01@gmail.com>
2023-02-18 06:02:55 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Prints the digits in base 10
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+
+	return (0);
+}
